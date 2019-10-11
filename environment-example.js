98cf20aefebdfce1c92e36eb870a61d452207eb9
iq 18 <ivan.quirke@mycit.ie>
2019-10-11 12:50:15 +0100

    project commit

---
+++ environment-example.js
@@ -0,0 +1,12 @@
+// Example environment.js file
+const auth = "Authentication-string";
+const mongoUser = "Database-Username"
+const mongoPass = "Database-Password"
+const dbName = "Database-Name"
+
+module.exports = {
+    auth: auth,
+    mongoUser: mongoUser,
+    mongoPass: mongoPass,
+    dbName: dbName
+}
